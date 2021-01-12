b0ef7f72cf8d96f6a8b53cba5318987ac0f16210
Codewell <69725306+codequicklearner@users.noreply.github.com>
2021-01-12 20:14:58 +0530

    Add files via upload
    Form validation using js

---
+++ JS form validation/app.js
@@ -0,0 +1,69 @@
+let userName=document.getElementById("txtUserName");
+let email=document.getElementById("txtEmail");
+let pwd=document.getElementById("txtPwd");
+let conPwd=document.getElementById("txtConPwd");
+let form=document.querySelector("form");
+
+function validateInput(){
+    //check username is empty 
+    if(userName.value.trim()===""){
+       onError(userName,"User Name cannot be empty");
+    }else{
+        onSuccess(userName);
+    }
+    if(email.value.trim()===""){
+        onError(email,"Email cannot be empty");
+    }else{
+        if(!isValidEmail(email.value.trim())){
+            onError(email,"Email is not valid");
+        }else{
+            onSuccess(email);
+        }
+    }
+
+    //password
+    if(pwd.value.trim()===""){
+        onError(pwd,"User Name cannot be empty");
+     }else{
+         onSuccess(pwd);
+     }
+     if(conPwd.value.trim()===""){
+        onError(conPwd,"User Name cannot be empty");
+     }else{
+         if(pwd.value.trim()!==conPwd.value.trim()){
+            onError(conPwd,"Password & Confirm password not matching");
+         }
+         else
+         onSuccess(conPwd);
+     }
+}
+
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateInput();
+});
+
+function onSuccess(input){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="hidden"; 
+    parent.classList.remove("error");
+    parent.classList.add("success");  
+}
+function onError(input,message){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="visible";
+    messageEle.innerText=message;  
+    parent.classList.add("error");
+    parent.classList.remove("success");
+
+}
+
+function isValidEmail(email){
+   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+
